dccd589d689f3627a33c9d14d27a44951b23c7de
Alexander Yan <frickenhamster@gmail.com>
2018-05-02 01:05:04 -0700

    add react-router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { NativeRouter, Route } from 'react-router-native';
+
+
 import AnimeMain from './modules/anime/containers/AnimeMain';
+
+
 
 export default class Main extends Component {
 	render() {
 		return (
 			<View style={{flex: 1}}>
-				<AnimeMain />
+				<NativeRouter >
+					<Route exact path='/' component={AnimeMain}/>
+				</NativeRouter>
 			</View>
 		)
 	}
