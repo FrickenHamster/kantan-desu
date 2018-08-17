cfd4cae38cda1f57634b319df70968bba3e241ec
Fricken Hamster <frickenhamster@gmail.com>
2018-08-17 00:52:52 -0700

    add android back button to router

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import createStore from './src/createStore';
 import Main from './src';
 import history from "./config/history";
 import { ConnectedRouter, push } from 'react-router-redux'
+import { BackButton } from 'react-router-native';
 import { PersistGate } from 'redux-persist/integration/react'
 
 const store = createStore();
@@ -18,9 +19,11 @@ export default class App extends React.Component {
 			>
 				<PersistGate persistor={store.persistor}>
 					<ConnectedRouter history={history}>
-						<View style={{flex: 1}}>
-							<Main/>
-						</View>
+						<BackButton>
+							<View style={{flex: 1}}>
+								<Main/>
+							</View>
+						</BackButton>
 					</ConnectedRouter>
 				</PersistGate>
 			</Provider>
